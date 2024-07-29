9082cada318984520360462d1540e52dc20fc4cc
Mohammed Shuhaib <shuhaib@theluxedevelopers.ae>
2024-07-29 15:56:50 +0400

    updates

--- app/Models/BankAccount.php
+++ app/Models/BankAccount.php
@@ -10,18 +10,7 @@ class BankAccount extends Model
     use HasFactory;
 
     protected $guarded = [];
-    // protected $fillable = [
-    //     'bank_name',
-    //     'account_number',
-    //     'account_holder_name',
-    //     'branch_address',
-    //     'swift_code',
-    //     'iban',
-    //     'account_type',
-    //     'currency',
-    // ];
 
-    // BankAccount.php
     public function invoices()
     {
         return $this->hasMany(Invoice::class);
